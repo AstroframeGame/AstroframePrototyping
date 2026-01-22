eb466c3e48004514fc8bd83117660538fdd3690e
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-01-21 20:12:36 -0800

    simple editor camera

---
+++ shipBuilding/editor_cam.gd
@@ -0,0 +1,6 @@
+extends Camera2D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if event.button_mask & MOUSE_BUTTON_MASK_MIDDLE:
+			position -= event.relative / zoom
