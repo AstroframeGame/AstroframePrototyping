3077f64d6a590ec3bfdc235776dca23a336e0ac2
Mont464 <amontore@ucsc.edu>
2026-01-28 15:09:37 -0800

    started work on player gun

---
+++ playerCombat/gun.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+#Following Guide: https://www.youtube.com/watch?v=FcNQII-d5Pg
+
+@onready var marker_2d: Marker2D = $Marker2D
+const bullet = preload("res://playerCombat/bullet.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+
+func shootBullet() -> void:
+	var new_bullet = bullet.instantiate()
+	new_bullet.position = marker_2d.global_position
+	new_bullet.target_position = (get_global_mouse_position() - marker_2d.global_position).normalized()
+	
