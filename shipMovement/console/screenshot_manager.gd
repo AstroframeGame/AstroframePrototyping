67339b1f62283cb6a01cc1ecd886426201868c0b
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-02-07 02:42:27 -0800

    web screenshot

--- shipMovement/console/screenshot_manager.gd
+++ shipMovement/console/screenshot_manager.gd
@@ -5,10 +5,18 @@ func _input(event):
 		capture_screenshot()
 
 func capture_screenshot():
+	await RenderingServer.frame_post_draw
 	var image = get_viewport().get_texture().get_image()
 	var time = Time.get_datetime_dict_from_system()
 	var file_name = "astroframe_screenshot_%04d%02d%02d_%02d%02d%02d.png" % [time.year, time.month, time.day, time.hour, time.minute, time.second]
-	image.save_png("user://" + file_name)
-	var real_path = ProjectSettings.globalize_path("user://" + file_name)
-	print("Image saved to " + real_path)
-	InGameConsole.log_message("Image saved to " + real_path)
+	
+	if OS.has_feature("web"):
+		var buffer = image.save_png_to_buffer()
+		JavaScriptBridge.download_buffer(buffer, file_name, "image/png")
+		InGameConsole.log_message("Screenshot downloaded: " + file_name)
+	else:
+		var path = "user://" + file_name
+		image.save_png(path)
+		var real_path = ProjectSettings.globalize_path(path)
+		print("Image saved to " + real_path)
+		InGameConsole.log_message("Image saved to " + real_path)
