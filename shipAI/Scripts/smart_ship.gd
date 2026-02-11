bbd23531171c1b057798c151fec31803cb3f15d2
Kevin Tan <astral.abyss7@gmail.com>
2026-02-11 13:50:57 -0800

    moved bee behaviors into smart_ship

--- shipAI/Scripts/smart_ship.gd
+++ shipAI/Scripts/smart_ship.gd
@@ -7,8 +7,13 @@ enum State {IDLE, APPROACHING, FLEEING, LATCHING, FLANKING, CIRCLING}
 @export var engine_thrust : float
 @onready var nav_agent : NavigationAgent2D = $NavigationAgent2D
 
+var piloting : Piloting
+var engines : Engines
+
 func _ready() -> void:
 	super._ready()
+	piloting = get_piloting()
+	engines = get_engines()
 	current_state = State.IDLE
 
 func _physics_process(_delta: float) -> void:
@@ -17,3 +22,23 @@ func _physics_process(_delta: float) -> void:
 ## update current_state based on target
 func update_state():
 	pass
+
+func sit_idle():
+	print("bee idling")
+
+func approach_target():
+	print("bee approaching")
+	var look_dir = nav_agent.get_next_path_position() - piloting.global_position
+	var target_angle = look_dir.angle() + PI/2
+	var angle_delta = wrapf(target_angle - global_rotation, -PI, PI)
+	angular_velocity = angle_delta * engines.rotational_thrust
+	linear_velocity -= transform.y * engines.standard_thrust
+	
+func latch_on(latching_position : Vector2):
+	print("bee latching")
+	global_position = target.to_global(latching_position)
+	var look_dir = -(piloting.global_position - target.global_position)
+	rotation = look_dir.angle() + PI/2 + PI
+
+func flee():
+	print("bee fleeing")
--- shipAI/bumbleBee/bumble_bee.gd
+++ shipAI/bumbleBee/bumble_bee.gd
@@ -1,35 +1,16 @@
 class_name Bumble_Bee
 extends Smart_Ship
 
-'''
-fly up to target if detected
-turn away and start shooting
-if target starts shooting or at least looking in direction of self
-then start fleeing while shooting
-'''
-
 @onready var detection_timer : Timer = $DetectionTimer
-## position relative to target when "latching"
 @export var latching_position : Vector2
 
-var engines : Engines
-var piloting : Piloting
-
 func _ready() -> void:
-	super._ready()
+	super()
 	detection_timer.timeout.connect(ship_detected)
-	engines = get_engines()
-	piloting = get_piloting()
 
-func _physics_process(delta: float) -> void:
-	'''
-	update_state()
-	move navAgent toward destination
-	change behavior based on state
-	'''
+func _physics_process(_delta: float) -> void:
 	if not engines or not piloting:
 		return
-
 	update_state()
 
 func update_state() -> void:
@@ -37,42 +18,22 @@ func update_state() -> void:
 		current_state = State.IDLE
 	else:
 		if nav_agent.is_target_reached():
-			# grab position relative to target -> latching_position
 			if current_state != State.LATCHING:
-				latching_position = target.to_local(global_position)
+				latching_position = target.to_local(piloting.global_position)
 			current_state = State.LATCHING
 		else:
-			nav_agent.target_position = target.global_position
+			nav_agent.target_position = target.get_piloting().global_position
 			current_state = State.APPROACHING
-		# if hp too low || if room is lost then flee TODO
 		
-	# react to state
 	match current_state:
 		State.IDLE:
-			print("bee idling")
+			sit_idle()
 		State.APPROACHING:
-			print("bee approaching")
-			if not nav_agent.is_navigation_finished():
-				var look_dir = nav_agent.get_next_path_position() - piloting.global_position
-				var target_angle = look_dir.angle() + PI/2
-				var angle_delta = wrapf(target_angle - global_rotation, -PI, PI)
-				angular_velocity = angle_delta * engines.rotational_thrust
-				linear_velocity -= transform.y * engines.standard_thrust
+			approach_target()
 		State.LATCHING:
-			print("bee latching")
-			# stop moving & lock position relative to player 
-			global_position = target.to_global(latching_position)
-			# point ass to player
-				# get position relative to self opposite of player
-			var look_dir = -(piloting.global_position - target.global_position)
-			#var target_angle = look_dir.angle() + PI/2
-			rotation = look_dir.angle() + PI/2 + PI
-				# ship.rotate_ship toward it
-				
-			# repeatedly shoot back cannon $Cannon.gun.shoot()
+			latch_on(latching_position)
 		State.FLEEING:
-			print("bee fleeing")
-			# fly forward and kinda fast
+			flee()
 
 func _on_detection_range_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player_ship"):
