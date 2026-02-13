843ea48d7e231e0eea1831b9f63daf793e14675c
Kevin Tan <astral.abyss7@gmail.com>
2026-02-12 23:07:14 -0800

    Reapply "Merge branch 'main' into kevin---ship-ai"
    This reverts commit 703af5e6801f89dae48fb0071f0fb9659a919d7e.

--- playerMovement/player.gd
+++ playerMovement/player.gd
@@ -51,7 +51,6 @@ var grounded : bool:
 	get:
 		ground_check = $GroundCheck
 		return ground_check.has_overlapping_bodies() or ground_check.has_overlapping_areas()
-		var health = 100
 
 
 @onready var handgun: PlayerGun = $handgun
