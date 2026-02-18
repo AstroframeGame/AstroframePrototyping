9506837ac793639a1400684ac7b27fa29edb7694
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-02-17 17:55:05 -0800

    fixed room visibility

--- shipBuilding/room_scripts/airlock.gd
+++ shipBuilding/room_scripts/airlock.gd
@@ -8,7 +8,7 @@ func on_door_interact(player : PlayerCharacter):
 	print(player.ship, ship)
 	if player.ship == ship:
 		player.on_ship_exit()
-		ship.on_airlock_interaction.emit(false)
+		ship.on_airlock_interaction.emit(player, false)
 	else:
 		player.on_ship_enter(ship)
-		ship.on_airlock_interaction.emit(true)
+		ship.on_airlock_interaction.emit(player, true)
