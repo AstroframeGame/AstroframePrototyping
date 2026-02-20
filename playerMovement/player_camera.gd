95a88dd93d8d7e4d90026d9de089d14f95d7716e
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-02-19 20:20:14 -0800

    tune rotation, camera turret

--- playerMovement/player_camera.gd
+++ playerMovement/player_camera.gd
@@ -33,7 +33,7 @@ func _physics_process(delta: float) -> void:
 		global_position = global_position.lerp(pos, pos_smooth * delta)
 		global_rotation = lerp_angle(global_rotation, player.ship.global_rotation, rot_smooth * delta)
 		zoom_goal = ship_zoom
-	elif seat and seat.room is Piloting:
+	elif seat and (seat.room is Piloting or seat.room is Turret):
 		var offset_px = get_viewport_rect().size * ship_flight_offset
 		offset_px += Vector2(0,seat.room.ship.get_bounds_rect().size.y/2)
 		var pos = player.ship.to_global(player.ship.center_of_mass) + offset_px.rotated(player.ship.global_rotation)
