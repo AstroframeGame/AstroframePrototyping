3e209195ed69b7aadd333bc515f7c81a7ded56dc
Kevin Tan <astral.abyss7@gmail.com>
2026-02-20 16:48:59 -0800

    shields have hp and recharge

--- shipBuilding/room_scripts/shields_room.gd
+++ shipBuilding/room_scripts/shields_room.gd
@@ -1,22 +1,35 @@
 extends Room
 class_name Shields_Room
 
+'''
+* shield holds take_damage & health
+* when shield dies, it stays down
+* start a timer based on recharge_speed
+* when timer ends, shield comes back up
+'''
 
-# should probably create a shield as a rb and pin it to the ship instead
-# not sure of the correct approach or what type of body it should be.
-
-# check power_level to change the shield recharge speed
-
-@export var recharge_speed = 30
+#@export var recharge_speed = 30
+@export var recharge_speeds := [-1, 7.5, 5, 2.5]
 @export var max_shield_durability = 100
 
 @onready var shield: Shield = $Shield
+@onready var recharge_timer : Timer = $RechargeTimer
 
 func  _ready() -> void:
 	on_power_level_change.connect(on_power_change)
+	shield.on_shield_broken.connect(recharge_shield)
+	recharge_timer.timeout.connect(deploy_shield)
 	on_power_change(self)
+	
+	shield.durability = max_shield_durability
 
 func on_power_change(_room):
-	recharge_speed = 10 * power_level
+	if shield.durability == max_shield_durability:
+		shield.set_active(power_level > 0)
+
+func recharge_shield():
+	recharge_timer.start(recharge_speeds[power_level])
+
+func deploy_shield():
+	shield.durability = max_shield_durability
 	shield.set_active(power_level > 0)
-	# smooth this later with a coroutine
--- turretInteractions/Scripts/shield.gd
+++ turretInteractions/Scripts/shield.gd
@@ -1,8 +1,22 @@
 class_name Shield
 extends StaticBody2D
 
+signal on_shield_broken()
+
 @onready var collider = $CollisionShape2D
 
+var durability : int
+
+## smooth this later with a coroutine
 func set_active(is_active : bool):
 	visible = is_active
 	collider.disabled = not is_active
+
+func take_damage(amount : int):
+	durability -= amount
+
+func _process(_delta: float) -> void:
+	if durability < 0:
+		set_active(false)
+		on_shield_broken.emit()
+		durability = 0
