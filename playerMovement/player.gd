fcc4ea3b51664b979e6b5b6d6812df8141fa71a4
TapeshSankaran <49103180+TapeshSankaran@users.noreply.github.com>
2026-02-25 15:55:40 -0800

    typo

--- playerMovement/player.gd
+++ playerMovement/player.gd
@@ -280,7 +280,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.is_action_pressed("interact") and not was_interacting:
 			interacting = true
 			was_interacting = true
-		elif !event.is_action_action("interact") and was_interacting:
+		elif !event.is_action_pressed("interact") and was_interacting:
 			was_interacting = false
 			
 		if seat and seat.room.has_method("handle_input"):
