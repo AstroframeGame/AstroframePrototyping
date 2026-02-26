023af559edfa5b918827ae5a2ba32abae93c87cb
Raven Ruiz <102258919+corvuscorae@users.noreply.github.com>
2026-02-25 20:03:14 -0800

    makrers get more opaque closer to target

--- encounter/encounters/__DEMO_LOCATION/0_1_DEMO/marker.gd
+++ encounter/encounters/__DEMO_LOCATION/0_1_DEMO/marker.gd
@@ -8,6 +8,11 @@ extends Polygon2D
 @onready var padding = 75
 @onready var y_offset = -10
 
+const FADE_MIN_DIST = 500
+const FADE_MAX_DIST = 3000
+
+var cam: Camera2D
+
 func _ready() -> void:
 	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	label.position = Vector2(-label.size.x / 2, y_offset)  
@@ -19,17 +24,17 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if not target: return
 	
-	var screen_pos = get_viewport().get_canvas_transform() * target.global_position
+	var target_screen_pos = get_viewport().get_canvas_transform() * target.global_position
 	
 	# if target is on screen, hide the marker
-	if Rect2(Vector2.ZERO, screen_size).has_point(screen_pos):
+	if Rect2(Vector2.ZERO, screen_size).has_point(target_screen_pos):
 		visible = false
 		return
 	
 	visible = true
 	
 	# get vector pointing from the center of the screen toward the target
-	var dir = screen_pos - center
+	var dir = target_screen_pos - center
 	
 	# walk from center to screen edge
 	var scale_x = (center.x - padding) / abs(dir.x) if dir.x != 0 else INF
@@ -37,6 +42,17 @@ func _process(_delta: float) -> void:
 	
 	# place marker
 	position = (center + dir * min(scale_x, scale_y)).round()
+	
+	# distance to target
+	if not cam:
+		cam = get_viewport().get_camera_2d()
+	var dist = target.global_position.distance_to(cam.global_position)
+	
+	# adjust alpha with distance
+	var t = remap(dist, FADE_MIN_DIST, FADE_MAX_DIST, 1, 0)
+	var dist_mod = smoothstep(0, 1, clamp(t, 0, 1))
+	self_modulate.a = dist_mod
+	label.modulate.a = dist_mod
 
 	# add text
 	if label.text == "" and target.get_meta("type"):
