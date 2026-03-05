fd8c924e30496f3893e7137a0c90e3528472bc0f
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-03-04 17:55:53 -0800

    powering on and off hex sound effects

--- shipBuilding/hex_scripts/input_hex.gd
+++ shipBuilding/hex_scripts/input_hex.gd
@@ -11,8 +11,17 @@ var is_powered : bool:
 			return room.ship.power_links.find_key(self) != null
 		return false
 
+# do not use this for reference
+var _was_powered = false
+
 # called in add room
 func update_state():
+	if _was_powered != is_powered:
+		if is_powered:
+			play_sfx(SFX_POWER_UP)
+		else:
+			play_sfx(SFX_POWER_DOWN)
+		_was_powered = is_powered
 	if not icon:
 		icon = $"Torus"
 	icon.self_modulate = Color("8effa8ff") if is_powered else Color("ec0083ff")
@@ -40,3 +49,19 @@ func interact(_player : PlayerCharacter) -> void:
 	if not can_interact():
 		return
 	on_clicked.emit(self)
+
+func _ready() -> void:
+	sfx.play()
+
+@onready var sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
+const SFX_POWER_UP = preload("res://audio/sfx/charge_short.wav")
+const SFX_POWER_DOWN = preload("res://audio/sfx/shields_down.wav")
+
+func play_sfx(fx):
+	if not sfx:
+		return
+	var polyphonic : AudioStreamPlaybackPolyphonic = sfx.get_stream_playback() as AudioStreamPlaybackPolyphonic
+	if fx and polyphonic:
+		polyphonic.play_stream(fx, 0,0,1, AudioServer.PLAYBACK_TYPE_DEFAULT, "SFX")
+	else:
+		push_warning("SFX tried to play but was not loaded")
