8632d2bfc0494c79c892a319a7083059296f1e4b
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-03-15 21:45:03 -0700

    Update game_manager.gd

--- hub/game_manager.gd
+++ hub/game_manager.gd
@@ -31,6 +31,12 @@ func load_scene(path : String)->void:
 	game_start.emit(current_scene)
 	MusicManager.play_gameplay()
 
+@rpc("authority", "call_local", "reliable")
+func load_scenes_across_peers(path):
+	multiplayer_manager.curr_scene_path = path
+	multiplayer_manager.is_in_scene = true
+	load_scene(path)	
+
 func start_game():
 	pass
 
