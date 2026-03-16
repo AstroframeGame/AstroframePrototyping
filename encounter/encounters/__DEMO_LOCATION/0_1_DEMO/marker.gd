c5258920f57c1b59e94d7bc6fa31e77e9a3967b6
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-03-15 22:03:41 -0700

    Revert "Merge branch 'logan-multiplayer-test-3-15-26' of https://github.com/AstroframeGame/AstroframePrototyping into logan-multiplayer-test-3-15-26"
    This reverts commit 10065b50628a8f5767bb9600f7a76c30b4e09e23, reversing
    changes made to c3d1c885bea19784a57eecfb5f8ff72dc33a20d7.

--- encounter/encounters/__DEMO_LOCATION/0_1_DEMO/marker.gd
+++ encounter/encounters/__DEMO_LOCATION/0_1_DEMO/marker.gd
@@ -59,10 +59,8 @@ func _process(_delta: float) -> void:
 	fill.modulate.a = dist_mod
 
 	# add text
-	#if label.text == "" and target.get_meta("type"):
-		#label.text = target.get_meta("type")
-	if label.text == "":
-		label.text = target.name.replacen("_"," ").to_pascal_case()
+	if label.text == "" and target.get_meta("type"):
+		label.text = target.get_meta("type")
 	
 	# keep label centered
 	label.pivot_offset = label.size / 2
