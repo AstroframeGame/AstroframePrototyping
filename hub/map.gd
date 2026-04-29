f5559cf2685e63dc4a452070f848dd9a574a7ba9
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-04-29 14:59:20 -0700

    more frameworking

---
+++ hub/map.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+class_name Map
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func generate_map():
+	pass
+	
+func start_level(level_path : String):
+	pass
