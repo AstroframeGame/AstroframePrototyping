e8d026530b44f2bf4b7a8f940c7d7f5b0a510416
Logan <34078184+SentientDragon5@users.noreply.github.com>
2026-05-01 21:13:02 -0700

    Squashed commit of the following:
    commit 65616692cdbf6a2ec0358557d1f36c640ff71947
    Author: Logan <34078184+SentientDragon5@users.noreply.github.com>
    Date:   Fri May 1 21:12:41 2026 -0700
    
        Update hub.tscn
    
    commit f1d7ec9fb9659bd6ed69f56156245ab99090676a
    Merge: 3d00837 b666827
    Author: Logan <34078184+SentientDragon5@users.noreply.github.com>
    Date:   Fri May 1 21:10:06 2026 -0700
    
        Merge branch 'main' into init-warping-4-29-26
    
    commit 3d00837eefa84a7ed73ec826a2f489a45839eaa0
    Merge: ed64913 959f373
    Author: Mont464 <amontore@ucsc.edu>
    Date:   Fri May 1 20:34:13 2026 -0700
    
        Merge branch 'main' into init-warping-4-29-26
    
    commit ed64913bfd7ee5fabd387462d79d2a0a5f4d1188
    Author: Mont464 <amontore@ucsc.edu>
    Date:   Fri May 1 20:31:26 2026 -0700
    
        despawn player when exiting level
    
    commit 1936f1924da3c1d24ac07905b734d11d5f7944e1
    Author: Mont464 <amontore@ucsc.edu>
    Date:   Wed Apr 29 18:31:16 2026 -0700
    
        back to map working, player not unloaded causing issues
    
    commit 08d0b43c282e3b5f0950a704000901fc3ca9263a
    Author: corvuscorae <corvuscorae@gmail.com>
    Date:   Wed Apr 29 17:54:03 2026 -0700
    
        can warp from map to build to scene (kind of)
    
    commit f5559cf2685e63dc4a452070f848dd9a574a7ba9
    Author: Logan <34078184+SentientDragon5@users.noreply.github.com>
    Date:   Wed Apr 29 14:59:20 2026 -0700
    
        more frameworking
    
    commit b422d3ce2e42ae34d2e53c8eadcab4c78e92c360
    Author: Logan <34078184+SentientDragon5@users.noreply.github.com>
    Date:   Wed Apr 29 14:37:37 2026 -0700
    
        start of roguelike design

---
+++ shipBuilding/in_game_ship_builder.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name InGameShipBuilder
+
+@onready var game_manager : GameManager = get_tree().root.get_node("Hub").get_node("GameManager")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	name = "InGameBuilder"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_warp_pressed() -> void:
+	game_manager.load_queued();
+	pass
